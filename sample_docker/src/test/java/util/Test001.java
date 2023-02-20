567f9616133e6f67cc60694daef7f40d658e8567
sundhareswar <sundharmathi26@gmail.com>
2023-02-20 22:05:24 +0530

    First Commit for testing

---
+++ sample_docker/src/test/java/util/Test001.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Test001 {
+
+	public static void test_new_001() {
+		System.out.println("Hello Sundhar");
+
+	}
+
+public static void main(String[] args) {
+	Test001 tc= new Test001();
+	tc.test_new_001();
+}
+}
